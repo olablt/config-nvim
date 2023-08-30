5cc29aac3c2f2cbd821d3c8d9a0a6ca1811eaa75
Melitonas <melitonas@gmail.com>
2023-08-30 07:43:40 +0300

    updated README.md with some instructions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # config-nvim
+
+```
+cd ~/.config/nvim/
+nvim lua/olab/packer.lua
+:so
+:PackerInstall
+```
+
+```
+:TSUpdate
+```
+
+LSP, install packages with `i`, uninstall with `X`
+```
+:Mason
+/gopls
+```
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,16 +2,16 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
--- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+-- -- Fix Undefined global 'vim'
+-- lsp.configure('lua-language-server', {
+--     settings = {
+--         Lua = {
+--             diagnostics = {
+--                 globals = { 'vim' }
+--             }
+--         }
+--     }
+-- })
 
 
 -- CMP
